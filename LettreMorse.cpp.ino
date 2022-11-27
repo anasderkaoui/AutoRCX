c52e50d2e12f5b3cf66bf66507ce15be65d1cfb7
Anas Derkaoui <115218309+anasderkaoui@users.noreply.github.com>
2022-11-27 22:41:34 +0100

    Add files via upload

---
+++ LettreMorse.cpp.ino
@@ -0,0 +1,207 @@
+#include "LettreMorse.h"
+#include <string.h>
+
+LettreMorse::LettreMorse(){};
+    
+
+const char* dot=" .";
+const char* dash="-";
+char lettremodifiee[10];
+
+//A  .-  B -...  C -.-.  D -.. E . F ..-.
+//G --. H ....  I ..  J .---  K -.- L .-..
+//M --  N -.  O --- P .--.  Q --.-  R .-.
+//S ... T - U ..- V ...-  W .-- X -..-
+//Y -.--  Z --..
+
+    
+void LettreMorse::convertisseur(char lettre){
+  
+if (lettre=='A'|| lettre == 'a')
+{
+strcat(lettremodifiee," . -");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='B'|| lettre == 'b')
+{
+strcat(lettremodifiee," - . . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='C'|| lettre == 'c')
+{
+strcpy(lettremodifiee," - . - . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='D'|| lettre == 'd')
+{
+strcpy(lettremodifiee," - . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='E'|| lettre == 'e')
+{
+strcpy(lettremodifiee," . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='F'|| lettre == 'f')
+{
+strcpy(lettremodifiee," . . - . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='G'|| lettre == 'g')
+{
+strcpy(lettremodifiee," - - . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='H'|| lettre == 'h')
+{
+strcpy(lettremodifiee," . . . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='I'|| lettre == 'i')
+{
+strcpy(lettremodifiee," . .");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='J'|| lettre == 'j')
+{
+strcpy(lettremodifiee," . - - -");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='K'|| lettre == 'k')
+{
+strcpy(lettremodifiee," - . -");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='L'|| lettre == 'l')
+{
+strcpy(lettremodifiee," . - . .");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='M'|| lettre == 'm')
+{
+strcpy(lettremodifiee," - -");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='N'|| lettre == 'n')
+{
+strcpy(lettremodifiee," - . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='O'|| lettre == 'o')
+{
+strcpy(lettremodifiee," - - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='P'|| lettre == 'p')
+{
+strcpy(lettremodifiee," . - - . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='Q'|| lettre == 'q')
+{
+strcpy(lettremodifiee," - - . - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='R'|| lettre == 'r')
+{
+strcpy(lettremodifiee," . - .");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='S'|| lettre == 's')
+{
+strcpy(lettremodifiee," . . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='T'|| lettre == 't')
+{
+strcpy(lettremodifiee," - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='U'|| lettre == 'u')
+{
+strcpy(lettremodifiee," . . - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='V'|| lettre == 'v')
+{
+strcpy(lettremodifiee," . . . - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='W'|| lettre == 'w')
+{
+strcpy(lettremodifiee," . - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='X'|| lettre == 'x')
+{
+strcpy(lettremodifiee," - . . - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='Y'|| lettre == 'y')
+{
+strcpy(lettremodifiee," - . - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='Z'|| lettre == 'z')
+{
+strcpy(lettremodifiee," - - . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='0')
+{
+strcpy(lettremodifiee," - - - - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre=='1')
+{
+strcpy(lettremodifiee," . - - - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '2')
+{
+strcpy(lettremodifiee," . . - - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '3')
+{
+strcpy(lettremodifiee," . . . - - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '4')
+{
+strcpy(lettremodifiee," . . . . - ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '5')
+{
+strcpy(lettremodifiee," . . . . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '6')
+{
+strcpy(lettremodifiee," - . . . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '7')
+{
+strcpy(lettremodifiee," - - . . . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '8')
+{
+strcpy(lettremodifiee," - - - . ");
+Serial.print(lettremodifiee);
+}
+else if (lettre == '9')
+{
+strcpy(lettremodifiee," - - - - . ");
+Serial.print(lettremodifiee);
+}
+else if(lettre == ' ')
+{
+Serial.print("/ ");
+}
+else{
+Serial.println(" Unknown symbol! LettreMorse ");;
+}
+}
---
+++ Traducteur.h
@@ -0,0 +1,17 @@
+#include "LettreMorse.h"
+//#ifndef TRADUCTEUR_H
+//#define TRADUCTEUR_H
+
+class Traducteur{
+  
+  
+  public:
+    
+    Traducteur();
+    void morse(char ch);  //Traduit la lettre en morse
+    void delay_alternative_dot();
+    void delay_alternative_dash();
+    
+};
+
+//#endif
