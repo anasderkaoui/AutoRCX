9f4a9e225d5b15e9fe4fc1b58e4e526cc5eca05d
Anas Derkaoui <115218309+anasderkaoui@users.noreply.github.com>
2022-11-29 22:54:46 +0100

    Update Traducteur.cpp

--- C++ & Arduino/Traducteur.cpp
+++ C++ & Arduino/Traducteur.cpp
@@ -1,7 +1,7 @@
+// Translate dots/dashes to LED light pulses on the Arduino UNO board
 
-
-#include "Traducteur.h"
-#include "LettreMorse.h"
+#include "Traducteur.h" // Includes the class "Traducteur.h" and all its functions
+#include "LettreMorse.h" // Includes the class "LettreMorse.h" and all its functions so that will not redefine it again
 
      
      
@@ -11,26 +11,26 @@ Traducteur::Traducteur(){}
 
      //int len;
      //char tab[]=" ";
-     const int led = 13;
-     // Variables will change:
-     int ledState = LOW;             // ledState used to set the LED
+     const int led = 13; // The corresponding pin to the LED of the Arduino UNO board
+     int ledState = LOW;     // ledState used to set the LED
      //int period_dot = 100;
      //int period_dash = 1000;
      //unsigned long current_time = 0;
+     // Generally, you should use "unsigned long" for variables that hold time
+     // The value will quickly become too large for an int to store
      unsigned long currentMillis = millis();
      unsigned long previousMillis = 0;        // will store last time LED was updated
 
      // constants won't change:
-     const long interval_dot = 100;           // interval at which to blink (milliseconds)
-     const long interval_dash = 1000;           // interval at which to blink (milliseconds)
+     const long interval_dot = 100;           // interval at which to blink if it is a dot (milliseconds)
+     const long interval_dash = 1000;           // interval at which to blink if it is a dash (milliseconds)
 
 void Traducteur::dot()
 {
-//while(millis() <= current_time + period_dot){
-//Serial.println("jj");
-if (currentMillis - previousMillis >= interval_dot) {
+if (currentMillis - previousMillis >= interval_dot) {  
+    // Blink the LED if the difference between the current time and last time you blinked the LED is bigger than the interval at which you want to blink the LED.
     // save the last time you blinked the LED
-    previousMillis = currentMillis;
+     previousMillis = currentMillis;
 
     // if the LED is off turn it on and vice-versa:
     if (ledState==LOW) {
@@ -42,9 +42,6 @@ if (currentMillis - previousMillis >= interval_dot) {
 }
 void Traducteur::dash()
 {
-//if(millis() >= current_time + period_dash){
-//Serial.println("jj");
-
 if (currentMillis - previousMillis >= interval_dash) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
@@ -58,34 +55,7 @@ if (currentMillis - previousMillis >= interval_dash) {
 }
 }
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-
-/*void loop() {
-  // here is where you'd put code that needs to be running all the time.
-
-  // check to see if it's time to blink the LED; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
-
-void Traducteur::dot()
-{
-//Serial.print(".");
-//digitalWrite(led, HIGH);
-delay_alternative_dot();
-//digitalWrite(led, LOW);
-//delay_alternative_dash();
-}
-void Traducteur::dash()
-{
-//Serial.print("-");
-//digitalWrite(led, HIGH);
-//delay_alternative_dash();
-//digitalWrite(led, LOW);
-delay_alternative_dash();
-}
-*/
-// Morse
+// Morse code for : Dots/Dashes -> LED Blink
 
 void Traducteur::morse(char ch[]){
 
