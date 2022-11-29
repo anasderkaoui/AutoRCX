ee379b9e60f2df49a1831abfce8a2dd3f4531874
Anas Derkaoui <115218309+anasderkaoui@users.noreply.github.com>
2022-11-29 22:42:55 +0100

    Update Traducteur.h

--- C++ & Arduino/Traducteur.h
+++ C++ & Arduino/Traducteur.h
@@ -1,18 +1,20 @@
-#include "LettreMorse.h"
+// Contains the functions that will translate dashes and dots to LED light pulses on the Arduino UNO board accordingly
+
+#include "LettreMorse.h" // Copies the content of the file "LettreMorse.h"
 //#ifndef TRADUCTEUR_H
 //#define TRADUCTEUR_H
 
 class Traducteur{
   
   
-  public:
+  public: // Functions that take the input characters in the main code and translate them to light pulses
     
     Traducteur();
-    void morse(char ch[]);  //Traduit la lettre en morse
+    void morse(char ch[]);  // Translates letters with the aid of "LettreMorse.h"
     //void delay_alternative_dot();
     //void delay_alternative_dash();
-    void dot();
-    void dash();
+    void dot(); // Translates dots to light pulses
+//     void dash();  // Translates dashes to light pulses
     
 };
 
