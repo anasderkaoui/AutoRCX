d67e973adf105fed2486e168def6ed35d02d96f9
Anas Derkaoui <115218309+anasderkaoui@users.noreply.github.com>
2022-11-29 22:37:29 +0100

    Update LettreMorse.h

--- C++ & Arduino/LettreMorse.h
+++ C++ & Arduino/LettreMorse.h
@@ -1,12 +1,14 @@
+// This is the class that will be converting letters to dashes and dots, the functions below will be defined in the file "LettreMorse.cpp"
+
 #include <Arduino.h>
-#ifndef LETTREMORSE_H
+#ifndef LETTREMORSE_H // Enables us to use/copy the content of "LettreMorse.h" in all of the other files that we will need
 #define LETTREMORSE_H
 
 class LettreMorse{
   
   
   
-public:
+public: // Functions that will be used in other files
 
   LettreMorse();
   void convertisseur(char lettre);
