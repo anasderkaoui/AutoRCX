5f28cf987db52f12dd64921cc353a4d65d372b87
Anas Derkaoui <115218309+anasderkaoui@users.noreply.github.com>
2022-11-29 11:09:29 +0100

    Add files via upload

---
+++ C++ & Arduino/LettreMorse.h
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#ifndef LETTREMORSE_H
+#define LETTREMORSE_H
+
+class LettreMorse{
+  
+  
+  
+public:
+
+  LettreMorse();
+  void convertisseur(char lettre);
+
+};
+
+#endif
